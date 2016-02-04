9000c45d628385344b44705859896242c473c3cd
SVladkov <s.v.vladkov@gmail.com>
2016-02-04 17:24:49 +0200

    Implemented the case when one of the searched people is a leader of the other.

--- Employees/EmployeesHierarchy.cs
+++ Employees/EmployeesHierarchy.cs
@@ -21,7 +21,7 @@ namespace EmployeesSystem
 
         private void FillData(String[] input)
         {
-            for (int i=2; i< input.Length; i++)
+            for (int i=2; i< input.Length - 1; i++)
             {
                 String[] inputAsArray = input[i].Split(new string[] { "-", "- ", " - ", " -", " " }, StringSplitOptions.None);
 
@@ -76,7 +76,6 @@ namespace EmployeesSystem
                         output.Append(employee + ", ");
                     }
                 }
-                //output.Remove(output.Length - 1, 1);
                 output.Length -= 2;
 
                 output.Append("\n");
@@ -92,6 +91,18 @@ namespace EmployeesSystem
             return lowestCommonLeader;
         }
 
+        private int IsWanted(String searchedEmployee, String firstEmployee, String secondEmployee)
+        {
+            if (searchedEmployee == firstEmployee || searchedEmployee == secondEmployee)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
         private int LowestCommonLeader(String searchedEmployee, String firstEmployee, String secondEmployee)
         {
             List<String> directEmployees;
@@ -102,51 +113,43 @@ namespace EmployeesSystem
             }
             else
             {
-                if (searchedEmployee == firstEmployee || searchedEmployee == secondEmployee)
-                {
-                    return 1;
-                }
-                else
-                {
-                    return 0;
-                }
+                return IsWanted(searchedEmployee, firstEmployee, secondEmployee);
             }
 
             switch (directEmployees.Count)
             {
                 case 0:
-                    if(searchedEmployee == firstEmployee || searchedEmployee == secondEmployee)
-                    {
-                        return 1;
-                    }
-                    else
-                    {
-                        return 0;
-                    }
+                    return IsWanted(searchedEmployee, firstEmployee, secondEmployee);
+
                 case 1:
-                    if (searchedEmployee == firstEmployee || searchedEmployee == secondEmployee)
+                    int foundBelow = LowestCommonLeader(directEmployees[0], firstEmployee, secondEmployee);
+
+                    if(foundBelow == 2 && lowestCommonLeader == null)
                     {
-                        return 1 + LowestCommonLeader(directEmployees[0], firstEmployee, secondEmployee);
+                        lowestCommonLeader = searchedEmployee;
+                        return foundBelow;
                     }
-                    else
-                    {
-                        return LowestCommonLeader(directEmployees[0], firstEmployee, secondEmployee);
-                    }
+
+                    return IsWanted(searchedEmployee, firstEmployee, secondEmployee)
+                        + foundBelow;
+
                 case 2:
                     int numberInLeftBranch = LowestCommonLeader(directEmployees[0], firstEmployee, secondEmployee);
                     int numberInRightBranch = LowestCommonLeader(directEmployees[1], firstEmployee, secondEmployee);
+
+                    if ((numberInLeftBranch == 2 || numberInRightBranch == 2) && lowestCommonLeader == null)
+                    {
+                        lowestCommonLeader = searchedEmployee;
+                        return 2;
+                    }
 
                     if(numberInLeftBranch == 1 && numberInRightBranch == 1)
                     {
                         lowestCommonLeader = searchedEmployee;
                         return 2;
                     }
-                    if(numberInLeftBranch + numberInRightBranch == 2)
-                    {
-                        return 2;
-                    }
 
-                    return numberInLeftBranch + numberInRightBranch;
+                    return IsWanted(searchedEmployee, firstEmployee, secondEmployee) + numberInLeftBranch + numberInRightBranch;
                 default:
                     return -1;
             }
